980d092b602bc9966932bab22f4e6ae1053a5140
Jasper Soleymanzadeh <jaspersoleymanzadeh@gmail.com>
2022-02-15 14:00:58 -0600

    added grade converter exercise

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class ControlFlowExercises {
     public static void main(String[] args) {
 //        int i = 5;
@@ -21,16 +23,77 @@ public class ControlFlowExercises {
 //        for (double j = 2; j < 1000000; j *= j) {
 //            System.out.printf("%d\n", (int) j);
 //        }
-        for (int i = 1; i <= 100; i++) {
-            if (i % 3 == 0 && i % 5 == 0) {
-                System.out.printf("FizzBuzz\n");
-            } else if (i % 3 == 0) {
-                System.out.printf("Fizz\n");
-            } else if (i % 5 == 0) {
-                System.out.printf("Buzz\n");
-            } else {
-                System.out.printf("%d\n", i);
+//        for (int i = 1; i <= 100; i++) {
+//            if (i % 3 == 0 && i % 5 == 0) {
+//                System.out.printf("FizzBuzz\n");
+//            } else if (i % 3 == 0) {
+//                System.out.printf("Fizz\n");
+//            } else if (i % 5 == 0) {
+//                System.out.printf("Buzz\n");
+//            } else {
+//                System.out.printf("%d\n", i);
+//            }
+//        }
+        Scanner scanner = new Scanner(System.in);
+//        System.out.print("Enter an integer: ");
+//        int userInt = scanner.nextInt();
+//        System.out.println("number\t|\tsquared\t|\tcubed\n------\t|\t------\t|\t------\n");
+//        for (int i = 1; i <= userInt; i++) {
+//            System.out.printf("%d\t\t|\t%d\t\t|\t%d\n", i, i * i, i * i * i);
+//        }
+//        do {
+//            System.out.print("Continue? [y/N] ");
+//            String userContinue = scanner.next();
+//            if (userContinue.equals("N")) {
+//                break;
+//            } else if (userContinue.equals("y")) {
+//                userInt++;
+//                System.out.printf("%d\t\t|\t%d\t\t|\t%d\n", userInt, userInt * userInt, userInt * userInt * userInt);
+//            } else {
+//                System.out.println("Invalid response.");
+//            }
+//        } while (true);
+        do {
+            System.out.print("Continue? [y/N] ");
+            String userContinue = scanner.next();
+            if (userContinue.equals("N")) {
+                System.out.println("Exiting...");
+                break;
+            } else if (!userContinue.equals("y")) {
+                System.out.println("Invalid response.");
+                continue;
             }
-        }
+            System.out.print("Enter a grade: ");
+            int userGrade = scanner.nextInt();
+            String convertedGrade;
+            if (userGrade >= 98) {
+                convertedGrade = "A+";
+            } else if (userGrade >= 93) {
+                convertedGrade = "A";
+            } else if (userGrade >= 90) {
+                convertedGrade = "A-";
+            } else if (userGrade >= 88) {
+                convertedGrade = "B+";
+            } else if (userGrade >= 83) {
+                convertedGrade = "B";
+            } else if (userGrade >= 80) {
+                convertedGrade = "B-";
+            } else if (userGrade >= 78) {
+                convertedGrade = "C+";
+            } else if (userGrade >= 73) {
+                convertedGrade = "C";
+            } else if (userGrade >= 70) {
+                convertedGrade = "C-";
+            } else if (userGrade >= 68) {
+                convertedGrade = "D+";
+            } else if (userGrade >= 63) {
+                convertedGrade = "D";
+            } else if (userGrade >= 60) {
+                convertedGrade = "D-";
+            } else {
+                convertedGrade = "F";
+            }
+            System.out.println(convertedGrade);
+        } while (true);
     }
 }

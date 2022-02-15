8cdc2e079dde3c648a8a36d5aa6debe2eda21c18
Jasper Soleymanzadeh <jaspersoleymanzadeh@gmail.com>
2022-02-15 12:13:01 -0600

    added while exercise

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -5,7 +5,7 @@ import static java.lang.Integer.parseInt;
 
 public class ConsoleExercises {
     public static void main(String[] args) {
-        double pi = 3.14159;
+//        double pi = 3.14159;
 //        System.out.printf("The value of pi is approximately %f", pi);
 //        System.out.format("The value of pi is approximately %f", pi);
         Scanner scanner = new Scanner(System.in);
